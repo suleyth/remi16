78d6cb6cba18db14882e30c9c1b2e8e8defe2957
suleyth <suleyth_c@pm.me>
2024-12-17 21:35:04 +0100

    Add ImGui

--- remi_debugger/main.cpp
+++ remi_debugger/main.cpp
@@ -18,25 +18,55 @@
 #include <remi_vm/vm.hpp>
 #include <SDL3/SDL.h>
 
+#include <imgui.h>
+#include "../vendor/imgui/backends/imgui_impl_sdl3.h"
+#include "../vendor/imgui/backends/imgui_impl_sdlrenderer3.h"
+
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
+    // Initialize window and renderer
     SDL_Window* window = SDL_CreateWindow("Remi16", 1280, 720, SDL_WINDOW_HIDDEN);
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderVSync(renderer, true);
+
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
 
     // Show window only after everything is loaded
     SDL_ShowWindow(window);
     bool running = true;
     while (running) {
+        // ---------------------------------------- Process system events
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
+            ImGui_ImplSDL3_ProcessEvent(&e);
             switch (e.type) {
             case SDL_EVENT_QUIT:
+            case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
                 running = false;
                 break;
             }
         }
+        // Don't update if the window is minimized
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+            SDL_Delay(10);
+            continue;
+        }
+        // Start the Dear ImGui frame
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
 
+
+        // ---------------------------------------- Update
+        ImGui::ShowDemoWindow(nullptr);
+
+        // ---------------------------------------- Draw
         // Clear to black
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
@@ -45,8 +75,17 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         auto test_rect = SDL_FRect { 100.0, 100.0, 100.0, 100.0 };
         SDL_RenderFillRect(renderer, &test_rect);
+
+        // Render ImGui
+        ImGui::Render();
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
